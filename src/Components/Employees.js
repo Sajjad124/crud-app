29fc29fdf2c25016dac51bde5d623738f7baebb4
Muhammad Umair <181370124@gift.edu.pk>
2022-11-07 15:07:09 +0500

    Initial commit

---
+++ src/Components/Employees.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// const Employees = () => {
+//   return (
+//     <div>Employees</div>
+//   )
+// }
+
+// export default Employees
+
+const Employees = [
+  {
+    id: "1",
+    name: "Sajjad",
+    age: "22",
+  },
+  {
+    id: "2",
+    name: "Shahzaib",
+    age: "22",
+  },
+  {
+    id: "3",
+    name: "Muhammad Umair",
+    age: "23",
+  },
+  {
+    id: "4",
+    name: "Muhammad Muzammil",
+    age: "22",
+  },
+];
