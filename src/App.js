81b4acfcf4339a805073d06717d4210785e4e0e9
Muhammad Umair <181370124@gift.edu.pk>
2022-11-07 22:31:08 +0500

    route working
    by using react-route-dome library

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
     </div>
