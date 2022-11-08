8457a439fd912a941554257d7f9308bfa0371061
Muhammad Umair <181370124@gift.edu.pk>
2022-11-08 17:42:39 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -29,15 +29,7 @@ const Home = () => {
             <td>
               <h4>Employees Table</h4>
             </td>
-            <td>
-              <input
-                style={{ padding: "5px", borderRadius: "10px" }}
-                placeholder="add new entry"
-              />
-            </td>
-            <td>
-              <Button>Add entry</Button>
-            </td>
+            
           </tr>
           <tr>
             <th>Name</th>
