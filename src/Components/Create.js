9ccecc2e4f60c9878ef122a4185085f07e6a0cd9
Muhammad Umair <181370124@gift.edu.pk>
2022-11-09 23:27:58 +0500

    Create Component
    Create component by using form, uuid, use navigate, button and useState

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Employees from "./Employees";
 import { v4 as uuid } from "uuid";
 import { Link, useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  let history = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = uuid();
+    let uniqueId = id.slice(0, 8);
+    let a = name;
+    let b = age;
+    Employees.push({ id: uniqueId, Name: a, Age: b });
+    history("/");
+  };
   return (
     <div>
       <form>
-        <input type="text" placeholder="Enter name" />
+        <input
+          type="text"
+          placeholder="Enter name"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter Age"
+          required
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <Button type="submit" onSubmit={(e) => handleSubmit(e)}>
+          Submit
+        </Button>
       </form>
     </div>
   );
